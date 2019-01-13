dc22611e13cc9becd31ff399fc98bd91af66369f
宁潇潇 <ningxiaoxiao@live.cn>
2019-01-13 23:22:48 +0800

    初始

--- NSGameDownloader/Form1.Designer.cs
+++ NSGameDownloader/Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_keyword = new System.Windows.Forms.TextBox();
+            this.button_search = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.radioButton_nsp = new System.Windows.Forms.RadioButton();
+            this.radioButton_xci = new System.Windows.Forms.RadioButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton_DLC = new System.Windows.Forms.RadioButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_keyword
+            // 
+            this.textBox_keyword.Location = new System.Drawing.Point(12, 12);
+            this.textBox_keyword.Name = "textBox_keyword";
+            this.textBox_keyword.Size = new System.Drawing.Size(342, 21);
+            this.textBox_keyword.TabIndex = 0;
+            this.textBox_keyword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_keyword_KeyPress);
+            // 
+            // button_search
+            // 
+            this.button_search.Location = new System.Drawing.Point(360, 12);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(63, 23);
+            this.button_search.TabIndex = 1;
+            this.button_search.Text = "搜索";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(441, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(589, 530);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // radioButton_nsp
+            // 
+            this.radioButton_nsp.AutoSize = true;
+            this.radioButton_nsp.Checked = true;
+            this.radioButton_nsp.Location = new System.Drawing.Point(12, 39);
+            this.radioButton_nsp.Name = "radioButton_nsp";
+            this.radioButton_nsp.Size = new System.Drawing.Size(41, 16);
+            this.radioButton_nsp.TabIndex = 3;
+            this.radioButton_nsp.TabStop = true;
+            this.radioButton_nsp.Text = "NSP";
+            this.radioButton_nsp.UseVisualStyleBackColor = true;
+            this.radioButton_nsp.CheckedChanged += new System.EventHandler(this.radioButton_nsp_CheckedChanged);
+            // 
+            // radioButton_xci
+            // 
+            this.radioButton_xci.AutoSize = true;
+            this.radioButton_xci.Location = new System.Drawing.Point(59, 39);
+            this.radioButton_xci.Name = "radioButton_xci";
+            this.radioButton_xci.Size = new System.Drawing.Size(41, 16);
+            this.radioButton_xci.TabIndex = 4;
+            this.radioButton_xci.TabStop = true;
+            this.radioButton_xci.Text = "XCI";
+            this.radioButton_xci.UseVisualStyleBackColor = true;
+            this.radioButton_xci.CheckedChanged += new System.EventHandler(this.radioButton_xci_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 61);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(411, 244);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "key";
+            this.columnHeader1.Width = 111;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "name";
+            this.columnHeader2.Width = 190;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 311);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(118, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 217);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // radioButton_DLC
+            // 
+            this.radioButton_DLC.AutoSize = true;
+            this.radioButton_DLC.Location = new System.Drawing.Point(106, 39);
+            this.radioButton_DLC.Name = "radioButton_DLC";
+            this.radioButton_DLC.Size = new System.Drawing.Size(65, 16);
+            this.radioButton_DLC.TabIndex = 9;
+            this.radioButton_DLC.TabStop = true;
+            this.radioButton_DLC.Text = "UPD+DLC";
+            this.radioButton_DLC.UseVisualStyleBackColor = true;
+            this.radioButton_DLC.CheckedChanged += new System.EventHandler(this.radioButton_DLC_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Silver;
+            this.linkLabel1.Location = new System.Drawing.Point(9, 528);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(217, 17);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "特别感谢 @ 91wii.riggzh  github.bob";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "type";
+            this.columnHeader3.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1042, 554);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.radioButton_DLC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.radioButton_xci);
+            this.Controls.Add(this.radioButton_nsp);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.textBox_keyword);
+            this.Name = "Form1";
+            this.Text = "NSGameDownloader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_keyword;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RadioButton radioButton_nsp;
+        private System.Windows.Forms.RadioButton radioButton_xci;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton_DLC;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- NSGameDownloader/Form1.cs
+++ NSGameDownloader/Form1.cs
@@ -1,20 +1,310 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using NSGameDownloader.Properties;
 
 namespace NSGameDownloader
 {
     public partial class Form1 : Form
     {
+        private const string PanUrlHead = "https://pan.baidu.com/s/";
+        private const string NspPanKey = "1tOFTvpJwikcdo2W12Z8dEw";
+        private const string XciPanKey = "1cwIw1-qsNOKaq6xrK0VUqQ";
+        private const string NspPw = "vb4v";
+        private const string XciPw = "fi4r";
+        private const string TitleKeysURL = "https://snip.li/nutdb";
+        private const string TitleKeysPath = "keys.json";
+        private string curTid;
+        private JObject Titlekeys;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private string GetUrl(string tid)
+        {
+            return PanUrlHead + (radioButton_xci.Checked ? XciPanKey : NspPanKey)
+                              + "#list/path=/"
+                              + (radioButton_xci.Checked ? "XCI" : "Nintendo Switch Games")
+                              + (radioButton_xci.Checked ? "" : radioButton_DLC.Checked ? "/UPD + DLC" : "/NSP")
+                              + "/" + tid.Substring(0, 5)
+                              + "/" + tid
+                              + "&parentPath=/";
+        }
+
+        public void UpdateTitleKey()
+        {
+            Titlekeys = new JObject();
+            var http = new WebClient { Encoding = Encoding.UTF8 };
+
+            ServicePointManager.ServerCertificateValidationCallback += delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var html = http.DownloadString(TitleKeysURL);
+
+            var keys = new List<string>(html.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+
+            //前3行 不要
+            keys.RemoveAt(0);
+            keys.RemoveAt(0);
+            keys.RemoveAt(0);
+
+            foreach (var key in keys)
+            {
+                var kan = key.Split('|');
+                //0100fbd00b91e0000000000000000005|2ef7a3ef88b25c3999cb82a489c526dd|Valkyria Chronicles 4 Demo
+
+                ///000结尾是本体
+                /// 800是upd
+                /// 别的是dlc
+                var jtemp = new JObject
+                {
+                    ["title"] = kan[0].Trim().Substring(0, 16),
+                    ["key"] = kan[2].Trim(),
+                    ["name"] = kan[6].Trim(),
+                    ["type"] = kan[3] == "1" ? "UPD" : kan[4] == "1" ? "DLC" : kan[5] == "1" ? "DEMO" : "BASE"
+                };
+
+                Titlekeys[kan[0].Trim()] = jtemp;
+            }
+
+
+            File.WriteAllText(TitleKeysPath, Titlekeys.ToString());
+        }
+
+
+
+
+        private void SetPw(string pw)
+        {
+            var element = webBrowser1.Document.GetElementById("nwtz3z5E");
+            if (element == null) return;
+            element.SetAttribute("value", pw);
+
+            var alla = webBrowser1.Document.GetElementsByTagName("a");
+            foreach (HtmlElement a in alla)
+                if (a.InnerText == "提取文件")
+                {
+                    a.InvokeMember("Click");
+                    break;
+                }
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var oUrl = WebUtility.UrlDecode(e.Url.ToString());
+
+
+            Console.WriteLine("log:" + oUrl);
+
+            if (webBrowser1.Document.Body.InnerText != null && webBrowser1.Document.Body.InnerText.Contains("请输入提取码"))
+            {
+                if (e.Url.ToString().Contains(NspPanKey.Substring(2))) SetPw(NspPw);
+                else if (e.Url.ToString().Contains(XciPanKey.Substring(2))) SetPw(XciPw);
+
+            }
+            else
+            {
+                if (oUrl == "https://pan.baidu.com/s/1tOFTvpJwikcdo2W12Z8dEw#list/path=/" ||
+                    oUrl == "https://pan.baidu.com/s/1cwIw1-qsNOKaq6xrK0VUqQ#list/path=/")
+                {
+                    WebRefresh();//输入密码后会再一次来到根目录,要再跳一次
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(TitleKeysPath))
+                UpdateTitleKey();
+            else
+                Titlekeys = JObject.Parse(File.ReadAllText(TitleKeysPath));
+
+            if (!Directory.Exists("image")) Directory.CreateDirectory("image");
+        }
+
+
+        private void radioButton_nsp_CheckedChanged(object sender, EventArgs e)
+        {
+            WebRefresh();
+        }
+
+        private void WebRefresh()
+        {
+            if (curTid == null) return;
+            Navigate(GetUrl(curTid));
+        }
+
+        private void radioButton_DLC_CheckedChanged(object sender, EventArgs e)
+        {
+            WebRefresh();
+        }
+
+        private void radioButton_xci_CheckedChanged(object sender, EventArgs e)
+        {
+            WebRefresh();
+        }
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+
+        private void Navigate(string url)
+        {
+            //todo 多线程
+
+            webBrowser1.Url = new Uri(url);
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            //var keys = Titlekeys.Root.Where(x => x.Contains(textBox_keyword.Text.Trim()));
+
+            listView1.Items.Clear();
+            foreach (var titlekey in Titlekeys)
+                //不显示demo
+                if (titlekey.Value["name"].ToString().Contains(textBox_keyword.Text.Trim()))
+                    listView1.Items.Add(new ListViewItem(new[]
+                    {
+                        titlekey.Value["title"].ToString(),
+                        titlekey.Value["name"].ToString(),
+                        titlekey.Value["type"].ToString()
+                    }));
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0) return;
+            curTid = listView1.SelectedItems[0].Text;
+
+            WebRefresh();
+
+
+            Console.WriteLine(curTid);
+
+            var g = Titlekeys[curTid].ToObject<JObject>();
+            if (!g.ContainsKey("info"))
+            {
+                var i = GetGameInfo(curTid);
+                if (i == null) return;
+                Titlekeys[curTid]["info"] = i;
+            }
+
+
+            var info = Titlekeys[curTid]["info"];
+
+
+            var size = info["total_rom_size"].ToObject<long>();
+
+            label2.Text = ConvertBytes(size) + "\n";
+            label2.Text += info["description"] + "\n";
+
+
+            var t = new Thread(GetGameImage);
+            t.Start(curTid);
+
+
+
+
+        }
+
+        public static string ConvertBytes(long len)
+        {
+            if (len > 1073741824)
+                return (len / 1024.0 / 1024 / 1024).ToString("F") + "GB";
+            if (len > 1048576)
+                return (len / 1024 / 1024).ToString("F") + "MB";
+            return (len / 1024).ToString("F") + "KB";
+        }
+
+        private void GetGameImage(object otid)
+        {
+            //todo 多线程
+            pictureBox1.Image = Resources.load;
+
+
+
+            var tid = otid as string;
+            var filename = "image\\" + tid + ".jpg";
+
+            if (File.Exists(filename))
+            {
+                pictureBox1.Image = Image.FromFile(filename);
+                return;
+            }
+
+            var web = new WebClient { Encoding = Encoding.UTF8 };
+            // 解决WebClient不能通过https下载内容问题
+            ServicePointManager.ServerCertificateValidationCallback += delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            try
+            {
+                web.DownloadFile("https://terannet.sirv.com/CDNSP/" + tid + ".jpg", filename);
+                pictureBox1.Image = Image.FromFile(filename);
+
+            }
+            catch
+            {
+                if (File.Exists(filename))
+                    File.Delete(filename);
+
+
+                pictureBox1.Image = Resources.error;
+                return;
+            }
+        }
+
+        private JObject GetGameInfo(string tid)
+        {
+            //todo 多线程
+            //https://ec.nintendo.com/apps/%s/AU
+            //todo 从http://www.eshop-switch.com 拿数据
+            using (var web = new WebClient() { Encoding = Encoding.UTF8 })
+            {
+                string html;
+                try
+                {
+                    html = web.DownloadString($"https://ec.nintendo.com/apps/{tid}/AU");
+                }
+                catch
+                {
+                    return null;
+                }
+
+                html = html.Split(new[] { "NXSTORE.titleDetail.jsonData = " }, StringSplitOptions.RemoveEmptyEntries)[1]
+                    .Split(new[] { "NXSTORE.titleDetail" }, StringSplitOptions.RemoveEmptyEntries)[0].Replace(";", "");
+
+                return JObject.Parse(html);
+            }
+        }
+
+        private void textBox_keyword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != (char)Keys.Enter) return;
+            e.Handled = true;
+            button_search_Click(null, null);
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.91wii.com/thread-104797-1-1.html");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(TitleKeysPath, Titlekeys.ToString());
         }
     }
 }
